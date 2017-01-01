2295c8f803f505a787ae7b7ac654f9e1cffa889e
TerryZjl <1538068051@qq.com>
2017-01-01 15:06:31 +0800

    maopao

--- test.c
+++ test.c
@@ -1,20 +1,37 @@
 #include<stdio.h>
-#include<stdio.h>
+#include<windows.h>
 
 int main()
 {
-	int money = 0;
-	printf("请输入钱数：");
-	scanf_s("%d", &money);
-	int bottle = money;
-	int num = money;
-	while (bottle>1)
-	{
+	int a[10];
+	int i = 0;
+	int j = 0;
+	int temp = 0;
 
-		num += bottle / 2;
-		bottle -= bottle / 2;
+	printf("请输入要排序的数字\n");
+	for (i = 0; i < 10; i++)
+	{
+		scanf_s("%d",&a[i]);
 	}
-	printf("可以喝：%d瓶\n", num);
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j < 9 - i; j++)
+		{
+			if (a[j]>a[j + 1])
+			{
+				temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+			}
+		}
+	}
+	printf("从小到大顺序依次是:\n");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
 	system("pause");
 	return 0;
 }

86f146377737adafe36c1cef967c9d51e9052dde
TerryZjl <1538068051@qq.com>
2017-01-14 18:07:45 +0800

    Konwn three people telling the truth,one unture A said:not me  B said :is C C said:is D D said:C is lying

---
+++ logic_suspect.c
@@ -0,0 +1,18 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+
+int main()
+{
+	char suspect;  //œ”“…∑∏
+	for (suspect = 'a'; suspect <= 'd'; suspect++)
+	{
+		if ((suspect != 'a') + (suspect == 'c') + (suspect == 'd') + (suspect != 'd') == 3)
+		{
+			printf("suspect is %c\n", suspect);
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}

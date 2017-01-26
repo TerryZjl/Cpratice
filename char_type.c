bc6353ad9287531929cd5a2b5152a04b2dfe8f71
TerryZjl <1538068051@qq.com>
2017-01-26 15:53:40 +0800

    char type

---
+++ char_type.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main()
+{
+	unsigned char a = -1;
+	signed char b= -1;
+	char c = -1;
+	printf("a=%d b=%d c=%d\n", a, b,c);
+
+	system("pause");
+	return 0;
+}

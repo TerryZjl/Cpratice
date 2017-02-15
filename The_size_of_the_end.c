a3496d9af544b7feec164b836272da70c47cf210
TerryZjl <1538068051@qq.com>
2017-02-15 16:12:09 +0800

    The size of the end storage

---
+++ The_size_of_the_end.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<windows.h>
+
+union hehe
+{
+	int a;
+	char c;
+}un;
+int main()
+{
+	un.a = 1;
+	if (un.c = 1)
+	{
+		printf("the small end storge\n");
+	}
+	else
+	{
+		printf("the small end storge\n");
+	}
+	system("pause");
+	return 0;
+}
